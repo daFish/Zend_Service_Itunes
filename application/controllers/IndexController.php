382d18e7768ed6e08db878dd859691c71c0761d9
daFish <dafish@soundtrack-board.de>
2011-04-02 11:57:41 +0200

    - misc changes

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -18,8 +18,8 @@ class IndexController extends Zend_Controller_Action
         
         $uc1 = new Zend_Service_Itunes_Search();
         $uc1->setTerms('angry birds')
-            ->setMediaType(Zend_Service_Itunes_ItunesAbstract::MEDIATYPE_SOFTWARE)
-            ->setResultFormat(Zend_Service_Itunes_Abstract::RESULT_ARRAY);
+            ->setEntity(array(Zend_Service_Itunes_ItunesAbstract::MEDIATYPE_SOFTWARE => 'software'))
+            ->setResultFormat(Zend_Service_Itunes_ItunesAbstract::RESULT_ARRAY);
         
         // query service and return new Zend_Service_Itunes_ResultSet object
         $results = $uc1->query();
